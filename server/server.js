ba70479845c7637bc751324acbeec38f892de66a
jungon <j017.kim@gmail.com>
2021-10-16 01:06:14 +0900

    add server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+
+  socket.on("send-message", ({ recipients, text }) => {
+    recipients.forEach((recipient) => {
+      const newRecipients = recipients.filter((r) => r !== recipient);
+      newRecipients.push(id);
+      socket.broadcast.to(recipient).emit("recieve-mesasge", {
+        recipients: newRecipients,
+        sender: id,
+        text,
+      });
+    });
+  });
+});
+
+httpServer.listen(5000);
